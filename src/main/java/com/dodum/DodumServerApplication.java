22adab3ba8d771028936765704cd388f755ae94f
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-08-21 19:47:36 +0900

    dodum 서버의 시작

---
+++ src/main/java/com/dodum/DodumServerApplication.java
@@ -0,0 +1,13 @@
+package com.dodum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodumServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodumServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dodum/server/DodumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodum.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodumServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
