f898b5d3b8719b68a64636dcb78c5b2fa3f599cb
daehyeong2 <baconbacon1231@gmail.com>
2025-10-15 23:51:37 +0900

    fix: getAllMisc Controller 반환값 ApiResponse 처리

--- src/main/java/BXND/dodum/domain/misc/presentation/MiscController.java
+++ src/main/java/BXND/dodum/domain/misc/presentation/MiscController.java
@@ -3,6 +3,7 @@ package BXND.dodum.domain.misc.presentation;
 import BXND.dodum.domain.misc.application.MiscApplicationUseCase;
 import BXND.dodum.domain.misc.application.data.req.GetAllMiscReq;
 import BXND.dodum.domain.misc.application.data.res.GetAllMiscRes;
+import BXND.dodum.global.data.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ public class MiscController {
   private final MiscApplicationUseCase miscApplicationUseCase;
 
   @GetMapping
-  public GetAllMiscRes getAllMisc(GetAllMiscReq getAllMiscReq){
-    return miscApplicationUseCase.getAllMisc(getAllMiscReq);
+  public ApiResponse<GetAllMiscRes> getAllMisc(GetAllMiscReq getAllMiscReq){
+    return ApiResponse.ok(miscApplicationUseCase.getAllMisc(getAllMiscReq));
   }
 }
